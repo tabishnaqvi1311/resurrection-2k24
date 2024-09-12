ca486d89e800e2e7a260f942bd9149d756a77504
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2024-09-12 23:44:16 +0530

    redeployement

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,7 +1,6 @@
-import { db } from "@/utils/db";
 import { NextRequest } from "next/server";
 
 export const GET = async (req: NextRequest) => {
-    const response = await db.event.findMany({});
-    return new Response(JSON.stringify(response), { status: 200});
+    // const response = await db.event.findMany({});
+    return new Response("my name jeff", { status: 200});
 }
